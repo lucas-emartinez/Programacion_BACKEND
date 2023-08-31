cb3291af944d2ac27bcfe337fa8029a20b9e650c
lucas-emartinez <lucas.martinez@terabytesrl.com.ar>
2023-08-31 00:24:25 -0300

    Entregable clase 3

--- Entregable clase3/ProductManager.js
+++ Entregable clase3/ProductManager.js
@@ -1,41 +1,70 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor(){
-        this.products = [];
+    constructor(path) {
+        this.path = path;
     }
 
-    addProduct(product){
+    async addProduct(product) {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            const productCodeExist = products.find(p => p.code === product.getCode());
+            
+            if (productCodeExist) return 'El Producto que intenta añadir tiene un código que ya existe';
 
-        // Tambien puedo poner if(!product.title || !product.stock || !product.price || !product.code || !product.thumbnail || !product.description)
-        if (!product.title) return 'El título es obligatorio';
-        if (!product.stock) return 'El stock es obligatorio';
-        if (!product.price) return 'El precio es obligatorio';
-        if (!product.code) return 'El código es obligatorio';
-        if (!product.thumbnail) return 'El thumbnail es obligatorio';
-        if (!product.description) return 'La descripción es obligatoria';
-
-        // Verifico el código
-        const codeExist = this.products.find(p => p.code === product.code);
-        if (codeExist){
-            return 'El código ya existe';
-        } ;
-
-        // Agrego el ID dependiendo si hay o no otros productos
-        const id = this.products.length ? this.products[this.products.length - 1].id + 1 : 1;
-        product.id = id;
-
-        this.products.push(product);
-        return 'Producto agregado con éxito';
-    }
-    
-    getProductById(id){
-        const product = this.products.find(p => p.id === id);
-        if (!product) return 'Not Found'
-        
-        return product;
+            const id = products.length ? products[products.length - 1].id + 1 : 1;
+            product.setId(id);
+            products.push(product);
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+            
+            return product;
+        } catch (error) {
+            console.log(`Error al leer o escribir el archivo: ${error}`);
+        }
     }
 
-    getProducts(){
-        return this.products;
+    async getProductById(id) {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            const product = products.find(product => product.id === id);
+            return product;
+        } catch (error) {
+            console.log(`Error al leer el archivo: ${error}`);
+        }
+    }
+
+    async getProducts() {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            return JSON.parse(data);
+        } catch (error) {
+            console.log(`Error al leer el archivo: ${error}`);
+        }
+    }
+
+    async updateProduct(id, product) {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            const oldProduct = products.find(p => p.id === id);
+            
+            if (!oldProduct) return 'No existe el producto';
+
+            oldProduct.title = product.getTitle();
+            oldProduct.stock = product.getStock();
+            oldProduct.price = product.getPrice();
+            oldProduct.code = product.getCode();
+            oldProduct.thumbnail = product.getThumbnail();
+            oldProduct.description = product.getDescription();
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+
+            return oldProduct;
+        } catch (error) {
+            console.log(`Error al actualizar el producto: ${error}`);
+        }
     }
 }
 
--- Entregable clase3/ProductManager_uso.js
+++ Entregable clase3/ProductManager_uso.js
@@ -1,33 +1,28 @@
 const ProductManager = require('./ProductManager');
+const ProductEntity = require('./ProductEntity');
 
-// Se creará una instancia de la clase “ProductManager”
-const productManager = new ProductManager();
+const productManager = new ProductManager("./productos.json");
 
-const product = {
-    title: 'Producto 1',
-    description: 'Descripción del producto',
-    price: 200,
-    thumbnail: 'Sin imagen',
-    code: 'abc123',
-    stock: 25
-};
+const product = new ProductEntity(
+    title = 'Producto 1',
+    description = 'Descripción del producto',
+    price = 200,
+    thumbnail = 'Sin imagen',
+    code = 'abc123',
+    stock = 25
+);
 
-// Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
-console.log(productManager.getProducts());
+(async () => {
+    // Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+    console.log(await productManager.getProducts());
 
-// Se llamará al método “addProduct” con los campos
-// El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
-console.log(productManager.addProduct(product))
+    // Se llamará al método “addProduct” con los campos
+    console.log(await productManager.addProduct(product));
 
-// Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
-console.log(productManager.getProducts())
+    // Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+    console.log(await productManager.getProducts());
 
-// Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
-console.log(productManager.addProduct(product))
+    // Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
+    console.log(await productManager.getProductById(1));
 
-// Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
-console.log(productManager.getProductById(1))
-
-// devolvera not Found ya que no existe
-console.log(productManager.getProductById(2));
-
+})();
