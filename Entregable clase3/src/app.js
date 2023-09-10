9c12b816f7107623ce4f14d5acad7cbfda5a14f2
lucas-emartinez <lucas.martinez@terabytesrl.com.ar>
2023-09-10 17:31:14 -0300

    Entrega servidor express

---
+++ Entregable clase3/src/app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+
+// Manager requerido
+const ProductManager = require("./impl/ProductManager");
+
+// Instanciacion de Express
+const app = express();
+
+// Instanciacion de Manager
+const productManager = new ProductManager(path.resolve(__dirname, "products.json"));
+
+// Definicion de puerto
+const PORT = 8000;
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Endpoints
+app.get("/", (req, res) => {
+    res.send(
+    `
+    <h1>Bienvenido al servidor.<h1>\n
+    <h2>Endpoints disponibles:</h2>\n
+    <h3>GET /products</h3>\n
+    <h3>GET /products/:pid</h3>\n
+
+    `);
+});
+
+app.get("/products", async (req, res) => {
+    const products = await productManager.getProducts();
+        
+    const { limit } = req.query;
+
+    if (limit) {
+        const productsLimit = products.slice(0, limit);
+        res.status(200).json(productsLimit);
+    } else {
+        res.status(200).json(products);
+    }
+});
+
+app.get("/products/:pid", async (req, res) => {
+
+    const { pid } = req.params;
+
+    if (!pid) {
+        res.status(400).json({error: 'Debe ingresar un id'});
+    }
+
+    const product = await productManager.getProductById(pid);
+    if (product === 'El producto no existe') {
+        res.status(404).json({error: 'El producto no existe'});
+    } else {
+        res.status(200).json(product);
+    }
+});
+
+
+app.listen(8000, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
